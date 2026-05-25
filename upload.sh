1423be58ea551c94338d2f9c64c86bfa049051ee
shun <truma.xiang@edoctor.cn>
2026-05-25 17:23:10 +0800

    build:0.3.4

--- upload.sh
+++ upload.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
 
+pod spec lint XSNetwork.podspec --allow-warnings --verbose --use-libraries --skip-import-validation
 
-pod spec lint --verbose XSNetwork.podspec
+pod trunk push XSNetwork.podspec --allow-warnings --verbose --use-libraries --skip-import-validation
 
-pod trunk push XSNetwork.podspec --allow-warnings --verbose --use-libraries
+#pod spec lint --verbose XSNetwork.podspec
+
+#pod trunk push XSNetwork.podspec --allow-warnings --verbose --use-libraries
 # pod repo push master XSNetwork.podspec --allow-warnings
